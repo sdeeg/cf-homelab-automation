3a2dcd784991b599072fa3d10d484adf8f75a59b
Scott Deeg <sdeeg@planet10.us>
2025-08-05 16:05:22 +0000

    move password to var

--- initialize-cf.sh
+++ initialize-cf.sh
@@ -1,4 +1,6 @@
-cf login -u admin -p TtJG49fUYVaFU-4l6K_T1hfsQP60_Rx8
+# Admin password from opsman
+CF_ADMIN_PASSWORD=TtJG49fUYVaFU-4l6K_T1hfsQP60_Rx8
+cf login -u admin -p $CF_ADMIN_PASSWORD
 
 cf create-org banzai
 cf create-space -o banzai dev
